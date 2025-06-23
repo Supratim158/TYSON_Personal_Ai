e90a3985530fe6a9c81aeec4bd43f73d0fada298
Supratim158 <supratim2005k@gmail>
2025-06-23 19:16:49 +0530

    adding csv to gitignore

--- test.py
+++
@@ -1,8 +0,0 @@
-# import pyautogui
-# import time
-
-# print("You have 5 seconds to move your mouse over the Send button...")
-# time.sleep(5)
-
-# x, y = pyautogui.position()
-# print(f"Mouse position: x={x}, y={y}")
