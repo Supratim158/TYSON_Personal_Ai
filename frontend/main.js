af51b0b0ceae19bd46a2ea6efa1e07c73ffe36a4
Supratim158 <supratim2005k@gmail>
2025-06-25 00:18:00 +0530

    Tyson for my laptop complete

--- frontend/main.js
+++ frontend/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    // eel.init()();  // Commented if not needed
+    eel.init()();  // Commented if not needed
 
     $('.text').textillate({
         loop: true,
--- main.py
+++ main.py
@@ -3,12 +3,30 @@ import eel
 
 from backend.features import *
 from backend.command import *
+from backend.auth import recoganize
 
 
 def start():
     eel.init('frontend')
 
     playAssistantSound()
+    
+    @eel.expose
+    def init():
+        subprocess.call([r'device.bat'])
+        eel.hideLoader()
+        speak("Ready For Face Authentication")
+        flag = recoganize.AuthenticateFace()
+        if flag ==1:
+            eel.hideFaceAuth()
+            speak("Face Authentication Successful")
+            eel.hideFaceAuthSuccess()
+            speak("Hello ,I am TYSON ur AI assistant")
+            eel.hideStart()
+            playAssistantSound()
+        else:
+            speak("Face Authentication Failed")
+    
 
     os.system('start msedge.exe --app="http://localhost:8000/index.html"')
 
